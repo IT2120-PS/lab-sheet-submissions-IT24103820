c139777e34e1c05d401a0b1eb7b6677385184dd4
IT24103820 <it24103820@my.sliit.lk>
2025-09-02 09:51:45 +0530

    IT24103820_Labsheet_06

---
+++ Lab Sheet 06/IT24103820.R
@@ -0,0 +1,26 @@
+getwd()
+setwd("C:\\Users\\IT24103820\\Desktop\\IT24103820")
+
+#EXERCISE
+
+#Question 01
+
+#Part 1
+#Binomial Distribution
+#Random variable x has binomial distribution with n=50 and p=0.85
+
+#Part 2
+pbinom(46, 50, 0.85, lower.tail = FALSE)
+
+
+#Question 02
+
+#Part 1
+#Number of calls in an hour
+
+#Part 2
+#Poisson Distribution
+#Random variable X has poisson distribution with lambda=12
+
+#Part 3
+dpois(15, 12)
